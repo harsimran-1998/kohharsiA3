616ed1aed3d5646fd264c228cfc4ca174d4281be
harsimran-1998 <52834777+harsimran-1998@users.noreply.github.com>
2019-07-30 16:55:12 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,11 @@
+import {booksInfo} from '../../app/booksInfo';
+
+
+export const MYBOOKS: booksInfo[] = 
+[
+{sbookn:'The Outsiders', sauthor: 'Francis Ford Coppola', sgenre: 'Comedy', syearpub: 2010 , spic :'assets/images/out.jpg'},
+{sbookn:'The Good Wife', sauthor: 'JP Delaney', sgenre: 'Modern Literature', syearpub: 1983, spic: 'assets/images/wife.jpg'},
+{sbookn:'The Stand', sauthor: 'Stephen King', sgenre: 'Drama', syearpub: 1903, spic: 'assets/images/stand.jpg'},
+{sbookn:'The Losers Club', sauthor: 'Andrew Clements', sgenre: 'Fantasy', syearpub: 1986 , spic: 'assets/images/losers.jpg'}
+
+]
